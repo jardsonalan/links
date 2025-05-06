b5089a32a515fae9ffc727d34bb97d18ba8ad874
Jardson Alan <jardsonalan132@gmail.com>
2025-05-06 18:32:37 -0300

    feat: created user information widget

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+import WidgetInfoUser from './views/WidgetInfoUser.vue';
 </script>
 
 <template>
-
+  <v-responsive>
+    <v-app class="bg-black">
+      <v-container class="w-100 bg-grey-darken-4 mt-8 rounded-lg" max-width="320" min-height="400">
+        <WidgetInfoUser/>
+      </v-container>
+    </v-app>
+  </v-responsive>
 </template>
 
 <style scoped>
---
+++ src/views/WidgetInfoUser.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <v-container class="bg-black rounded-lg">
+    <v-row>
+      <v-col cols="3">
+        <!-- Imagem de perfil -->
+        <v-img
+          src="#"
+          :width="300"
+          cover
+        />
+      </v-col>
+      <v-col cols="9">
+        <!-- Nome e Stack -->
+        <span class="d-block text-subtitle-2 font-weight-bold">Jardson Alan</span>
+        <span class="d-block text-caption">Desenvolvedor de Software</span>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+</style>
