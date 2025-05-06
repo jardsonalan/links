64b02cbab0f3e7dfedab63d546cfb3fc5e28de66
Jardson Alan <jardsonalan132@gmail.com>
2025-05-06 19:20:48 -0300

    feat: created hard skills widget

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import WidgetInfoUser from './views/WidgetInfoUser.vue';
+import WidgetHardSkills from './views/WidgetHardSkills.vue';
 </script>
 
 <template>
   <v-responsive>
     <v-app class="bg-black">
       <v-container class="w-100 bg-grey-darken-4 mt-8 rounded-lg" max-width="320" min-height="400">
-        <WidgetInfoUser/>
+        <v-row>
+          <v-col cols="12">
+            <WidgetInfoUser/>
+          </v-col>
+          <v-col cols="12">
+            <WidgetHardSkills/>
+          </v-col>
+        </v-row>
       </v-container>
     </v-app>
   </v-responsive>
---
+++ src/views/WidgetHardSkills.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <v-container class="bg-black rounded-lg">
+    <v-row>
+      <v-col cols="12">
+        <span class="text-subtitle-2 font-weight-bold">Hard Skills</span>
+      </v-col>
+      <v-col cols="12" class="text-center mt-n3">
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuetify/vuetify-original.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-plain.svg" class="hard-skills-icons"/>
+          <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" class="hard-skills-icons"/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+.hard-skills-icons {
+  width: 19px;
+  margin-right: 4px;
+}
+</style>
